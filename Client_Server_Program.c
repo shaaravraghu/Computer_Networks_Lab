906033787811ce5e8ceba1b1a1d01944371efa26
Shaarav Raghu <shaaravraghu@gmail.com>
2026-08-04 15:11:04 +0530

    Include headers for socket programming
    Added necessary header files for socket programming.

--- Client_Server_Program.c
+++ Client_Server_Program.c
@@ -1,1 +1,6 @@
-
+#include <stdio.h> // Provides printf(), fprintf(), fgets(), perror(), stdin and
+stderr
+#include <sys/types.h> // Defines system data types used in socket programming
+#include <sys/socket.h> // Provides socket(), connect(), send() and socket constants
+#include <netinet/in.h> // Provides sockaddr_in, AF_INET and htons()
+#include <netdb.h> // Provides gethostbyname() and struct hostent
