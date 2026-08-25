e21a6ba3f4dd65f3d39e4c922787f2134f5ff349
SHAARAV RAGHU <195157135+shaaravraghu@users.noreply.github.com>
2026-08-25 16:01:34 +0530

    Implement IPC with message queues and fork
    Added inter-process communication using message queues and fork.

--- InterProcess_Communication.c
+++ InterProcess_Communication.c
@@ -2,6 +2,8 @@
 #include <sys/stat.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
+#include <unistd.h>
+#include <sys/types.h>
 
 
 mkfifo(const char *pathname, mode_t mode);
@@ -10,3 +12,14 @@ msgid = msgget(key, 0666 | IPC_CREAT); // msgget creates a message queue and ret
 msgsnd(msgid, &message, sizeof(message), 0); // msgsnd to send message
 msgrcv(msgid, &message, sizeof(message), 1, 0); // msgrcv to receive message
 msgctl(msgid, IPC_RMID, NULL); // msgctl to destroy the message queue
+
+// Functions for fork() in C/ C++
+pid_t fork(void);
+pid_t pid = fork();
+  if (pid == 0) {
+  // child process — e.g. Vikram
+} else if (pid > 0) {
+  // parent process — e.g. Arjun
+} else {
+  perror("fork failed");
+}
